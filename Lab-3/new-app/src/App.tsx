a9f4c107eb7051deaa398d0f373028b4f9e8fa60
arina22222 <106924080+arina22222@users.noreply.github.com>
2023-03-31 13:21:09 +0300

    dwcd

--- Lab-3/new-app/src/App.tsx
+++ Lab-3/new-app/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AppBar, Toolbar, Container, Box, Typography } from '@material-ui/core';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+        <Typography variant="h6">My App</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md">
+      <Typography variant="body1">Welcome to my app!</Typography>
+      </Container>
+    </Box>
+  
   );
 }
+
 
 export default App;
--- Lab-3/new-app/src/index.tsx
+++ Lab-3/new-app/src/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Roboto',
+      'sans-serif'
+    ].join(','),
+  },
+});
+
+function MyApp() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* Restul componentelor tale */}
+    </ThemeProvider>
+  );
+}
+
+ReactDOM.render(
+  <MyApp />,
+  document.getElementById('root')
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
