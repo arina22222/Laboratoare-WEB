e0ca91534d9bc28ffd7f0a434639277e6ce4d038
arina22222 <106924080+arina22222@users.noreply.github.com>
2023-05-07 14:38:31 +0300

    Update App component

--- lab7/new-app/src/App.tsx
+++ lab7/new-app/src/App.tsx
@@ -1,25 +1,27 @@
 import * as React from 'react';
 import { Link} from 'react-router-dom';
 import Main from './Main';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { SwapOutlined, PlayCircleOutlined, DownSquareOutlined } from '@ant-design/icons';
 import type { MenuProps, MenuTheme } from 'antd';
 import { Menu, Switch } from 'antd';
-
+import './App.css'
 
 export default function App() {
 
-
-
-
   return (
     <>  
-      <div>
-
-        <ul>
-          <li><Link to='/'>About today </Link></li>
-          <li><Link to='/topics'>Now</Link></li>
-          <li><Link to='/settings'>About Tomorrow</Link></li>
-        </ul>
+      <div className='Main'>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<SwapOutlined />}>
+            <Link to='/'>About today</Link>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<PlayCircleOutlined />}>
+            <Link to='/topics'>Now</Link>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<DownSquareOutlined />}>
+            <Link to='/settings'>About Tomorrow</Link>
+          </Menu.Item>
+        </Menu>
         <hr />
         <Main />       
       </div>   
